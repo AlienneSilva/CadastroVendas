346c2b1c0caa3f8d30828745a876722228f77fc6
AlienneSilva <88863077+AlienneSilva@users.noreply.github.com>
2026-01-19 15:14:17 -0300

    Atualização do commit do projeto de cadastro

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 "# CadastroVendas" 
+"Cadastro de vendas
+Requisitos funcionais do usuário;
+1.	usuário deve ser capaz de realizar login;
+2.	Usuário deve ser capaz de criar um cadastro básico com os dados da venda;
+3.	Usuário deve ser capaz de salvar os dados;
+4.	Usuário deve ser capaz de visualizar as suas vendas;
+5.	Usuário deve ser capaz de atualizar dados do “card”;
+Requisitos funcionais do sistema;
+1.	Tela inicial de login e senha;
+2.	Menu lateral com:
+•	Cadastro;
+•	Minhas vendas;
+Tela de Cadastro:
+•	Nome do cliente (empresa);
+•	Nome do responsável;
+•	Número de vidas;
+•	Produto;
+•	Cidade;
+Tela de vendas
+•	Número identificação do card;
+•	Status;
+"
---
+++ js/main.js
@@ -0,0 +1,39 @@
+const container_form = {}
+
+
+const CADASTRO = {}
+
+function container_form(form){
+
+    ``
+
+    return form
+}
+
+/* function form(form){
+ const FORM = document.getElementById("cadastroForm");
+
+    FORM.addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      const formData = new FormData(FORM);
+
+      fetch("https://script.google.com/macros/s/AKfycbxpkq8Hf5gND2CR_slUr-FvwgcTTa2GR_QXoyebOMp4fIwlq6VBOLWe7_wk2SSwWMp4/exec", {
+        method: "POST",
+        body: formData
+      })
+      .then(res => res.text())
+      .then(res => {
+        if (res.includes("OK")) {
+          alert("Cadastro enviado com sucesso!");
+          // Recarrega a página, COLOCAR AS VARIAVEIS CONSTANTES COM LETRA MAIUSULA
+          window.location.reload();
+        } else {
+          alert("Erro ao salvar: " + res);
+        }
+      })
+      .catch(err => {
+        alert("Erro na conexão com o servidor.");
+        console.error(err);
+      });
+    })*/
